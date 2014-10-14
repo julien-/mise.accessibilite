f5008d31a9a646c253ddb0483836d82d52254629
damien57050 <maxence57>
2014-10-14 13:28:48 +0200

    Seance_precedente

--- etudiant/Vue/seance_precedente.php
+++ etudiant/Vue/seance_precedente.php
@@ -1,7 +1,11 @@
 <?php 
-print_r($listeAvancement);
-/*foreach ($listeAvancement as $avancement)
+foreach ($listeAvancement as $avancement)
 {
-	echo $avancement['fait'];
-}*/
+	echo "Titre theme : ".$avancement['exercice']['theme']['titre'].'  //  ';
+	echo "Titre exercice : ".$avancement['exercice']['titre'].'  //  ';
+	echo "Fait : ".$avancement['fait'].'  //  ';
+	echo "Compris : ".$avancement['compris'].'  //  ';
+	echo "Assimile: ".$avancement['assimile'];
+	echo "<br/>";
+}
 ?>
