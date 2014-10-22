e6562fdd858058eb101bce7cdecd6679d0783745
damien57050 <maxence57>
2014-10-22 16:26:59 +0200

    Index/Etudiant

--- modele/sql/DAOAvancement.php
+++ modele/sql/DAOAvancement.php
@@ -77,164 +77,184 @@ class DAOAvancement extends DAOStandard
 		return number_format(($avancement['progression'] / $avancement['total']) * 100, 2);
 	}
 	
-  function getByTheme($idTheme)
-  {
-  	$sql = 'CREATE TEMPORARY TABLE R1
+  	function getByTheme($idTheme)
+  	{
+  		$sql = 'CREATE TEMPORARY TABLE R1
         SELECT COUNT(*) * 100 as total
         FROM exercice
         WHERE id_theme = ' . $idTheme;
   	
-  	$this->executeQuery($sql);
-  	
-  	$sql = 'CREATE TEMPORARY TABLE R2
-        SELECT SUM(fait+compris+assimile) as progression
-        FROM theme t, avancement a, exercice e
-        WHERE t.id_theme = e.id_theme
-        AND e.id_exo = a.id_exo
-        AND t.id_theme = ' . $idTheme;
-  	
-  	$this->executeQuery($sql);
-  	
-  	$sql = 'CREATE TEMPORARY TABLE R3
-            SELECT COUNT(*) as nbEtudiants
-            FROM etudiant';
-  	
-  	$this->executeQuery($sql);
-  	
-  	$sql = 'SELECT progression, total * nbEtudiants as total
-        FROM R3, R2, R1';
-  	
-  	$result = $this->_db->query($sql) or die (mysql_error());
-  	
-  	$avancement = $this->fetchArray($result);
+	  	$this->executeQuery($sql);
+	  	
+	  	$sql = 'CREATE TEMPORARY TABLE R2
+	        SELECT SUM(fait+compris+assimile) as progression
+	        FROM theme t, avancement a, exercice e
+	        WHERE t.id_theme = e.id_theme
+	        AND e.id_exo = a.id_exo
+	        AND t.id_theme = ' . $idTheme;
+	  	
+	  	$this->executeQuery($sql);
+	  	
+	  	$sql = 'CREATE TEMPORARY TABLE R3
+	            SELECT COUNT(*) as nbEtudiants
+	            FROM etudiant';
+	  	
+	  	$this->executeQuery($sql);
+	  	
+	  	$sql = 'SELECT progression, total * nbEtudiants as total
+	        FROM R3, R2, R1';
+	  	
+	  	$result = $this->_db->query($sql) or die (mysql_error());
+	  	
+	  	$avancement = $this->fetchArray($result);
+		
+	  	if ($avancement['total'] == 0)
+	  		return 0;
+	  	
+	  	return number_format(($avancement['progression'] / $avancement['total']) * 100, 2);
+   	}
+  
+  	function getByCours($idCours)
+  	{
+	  	$sql = '    SELECT e.id_exo '
+	  			. ' FROM exercice e, theme t '
+	  			. ' WHERE t.id_theme = e.id_theme '
+	  					. ' AND id_cours = ' . $idCours;
+	  	
+	  	$reqTotal = $this->executeQuery($sql);
+	  	$total = $this->countRows($reqTotal);
+	  	
+	  	$sql = 'SELECT e.id_etu FROM etudiant e';
+	  	$reqTotalEtudiant = $this->executeQuery($sql);
+	  	$totalEtudiant = $this->countRows($reqTotalEtudiant);
+	  	
+	  	$total = $total * $totalEtudiant * 100;
 	
-  	if ($avancement['total'] == 0)
-  		return 0;
-  	
-  	return number_format(($avancement['progression'] / $avancement['total']) * 100, 2);
-  }
-  
-  function getByCours($idCours)
-  {
-  	
-  	$sql = '    SELECT e.id_exo '
-  			. ' FROM exercice e, theme t '
-  			. ' WHERE t.id_theme = e.id_theme '
-  					. ' AND id_cours = ' . $idCours;
-  	
-  	$reqTotal = $this->executeQuery($sql);
-  	$total = $this->countRows($reqTotal);
-  	
-  	$sql = 'SELECT e.id_etu FROM etudiant e';
-  	$reqTotalEtudiant = $this->executeQuery($sql);
-  	$totalEtudiant = $this->countRows($reqTotalEtudiant);
-  	
-  	$total = $total * $totalEtudiant * 100;
-
-  	$sql = 'SELECT SUM( assimile + compris + fait ) AS progression
-            FROM avancement a, theme t, exercice e
-            WHERE a.id_exo = e.id_exo
-            AND t.id_theme = e.id_theme
-            AND id_cours = ' . $idCours;
-  	
-  	$reqProgression = $this->executeQuery($sql);
-
-  	$avancement = $this->fetchArray($reqProgression);
-  	
-  	if ($total == 0)
-  		return 0;
-  	
-  	return number_format(($avancement['progression'] / $total) * 100, 2);
-  }
-  
-  function getByCoursEtudiant($idCours, $idEtudiant)
-  {
-  	$sql = 'SELECT (SUM(fait+compris+assimile) / (count(e.id_exo) * 100)) * 100 as progression
-			FROM theme t, exercice e
-			LEFT JOIN avancement a ON e.id_exo = a.id_exo
-			AND id_etu = ' . $idEtudiant.'
-			WHERE t.id_cours = ' . $idCours.'
-			AND e.id_theme = t.id_theme
-			GROUP BY t.id_cours';
-  	
-  	$req_progression = $this->executeQuery($sql);
-  	$avancement = $this->fetchArray($req_progression);
-  	
-  	return number_format($avancement['progression'], 2);
-  }
-  
-  function getBySeanceEtudiant($idSeance, $idEtudiant)
-  {
-  	$result = $this->executeQuery('SELECT * FROM avancement, theme, exercice 
-  			WHERE avancement.id_etu = ' . $idEtudiant . '
-			AND avancement.id_seance = ' . $idSeance .'
-			AND avancement.id_exo = exercice.id_exo
-  			AND exercice.id_theme = theme.id_theme');
-  	$listeAvancement = array();
-  	while ($avancement = $this->fetchArray($result)) {
-  	
-  			$listeAvancement[] = array('exercice' => array('id' => $avancement['id_exo'],
-															'titre' => $avancement['titre_exo'],
-															'numero' => $avancement['num_exo'],
-															'theme' => array ('id' => $avancement['id_theme'],
-																				'titre' => $avancement['titre_theme'])),
-  										'fait' => $avancement['fait'],
-  										'compris' => $avancement['compris'],
-  										'assimile' => $avancement['assimile']);
+	  	$sql = 'SELECT SUM( assimile + compris + fait ) AS progression
+	            FROM avancement a, theme t, exercice e
+	            WHERE a.id_exo = e.id_exo
+	            AND t.id_theme = e.id_theme
+	            AND id_cours = ' . $idCours;
+	  	
+	  	$reqProgression = $this->executeQuery($sql);
+	
+	  	$avancement = $this->fetchArray($reqProgression);
+	  	
+	  	if ($total == 0)
+	  		return 0;
+	  	
+	  	return number_format(($avancement['progression'] / $total) * 100, 2);
   	}
-  	return $listeAvancement;
-  }
   
-  function getTabByCoursEtudiant($idCours, $idEtudiant)
-  {
-  	$result = $this->executeQuery('SELECT * FROM avancement, theme, exercice, seance
-  			WHERE avancement.id_etu = ' . $idEtudiant . '
-			AND avancement.id_exo = exercice.id_exo
-  			AND exercice.id_theme = theme.id_theme
-  			AND theme.id_cours = ' . $idCours);
-  	
-  	$listeAvancement = array();
-  	while ($avancement = $this->fetchArray($result)) {
-  		 
-  		$listeAvancement[] = array('exercice' => array('id' => $avancement['id_exo'],
-  				'titre' => $avancement['titre_exo'],
-  				'numero' => $avancement['num_exo'],
-  				'theme' => array ('id' => $avancement['id_theme'],
-  						'titre' => $avancement['titre_theme'])),
-  				'fait' => $avancement['fait'],
-  				'compris' => $avancement['compris'],
-  				'assimile' => $avancement['assimile']);
+  	function getByCoursEtudiant($idCours, $idEtudiant)
+  	{
+	  	$sql = 'SELECT (SUM(fait+compris+assimile) / (count(e.id_exo) * 100)) * 100 as progression
+				FROM theme t, exercice e
+				LEFT JOIN avancement a ON e.id_exo = a.id_exo
+				AND id_etu = ' . $idEtudiant.'
+				WHERE t.id_cours = ' . $idCours.'
+				AND e.id_theme = t.id_theme
+				GROUP BY t.id_cours';
+	  	
+	  	$req_progression = $this->executeQuery($sql);
+	  	$avancement = $this->fetchArray($req_progression);
+	  	
+	  	return number_format($avancement['progression'], 2);
   	}
-  	return $listeAvancement;
-  }
   
-  function getTabByThemeEtudiant($idTheme, $idEtudiant)
-  {
-  	/*$result = $this->executeQuery('SELECT * FROM avancement, theme, exercice, seance
-  			WHERE avancement.id_etu = ' . $idEtudiant . '
-			AND avancement.id_exo = exercice.id_exo
-  			AND exercice.id_theme = '. $idTheme);*/
-  	
-  	$result = $this->executeQuery('SELECT e.id_exo, titre_exo, num_exo, e.id_theme, titre_theme, COALESCE(fait, 0) AS fait, COALESCE(compris, 0) AS compris, COALESCE(assimile, 0) AS assimile
-  	FROM theme t, exercice e
-  	LEFT JOIN avancement a ON e.id_exo = a.id_exo
-  	AND id_etu = '.$idEtudiant.'
-  	WHERE e.id_theme = '.$idTheme.'
-  	GROUP BY e.id_exo');
-  	
-  	$listeAvancement = array();
-  	
-  	while ($avancement = $this->fetchArray($result)) {
-  			
-  		$listeAvancement[] = array('exercice' => array('id' => $avancement['id_exo'],
-  				'titre' => $avancement['titre_exo'],
-  				'numero' => $avancement['num_exo'],
-  				'theme' => array ('id' => $avancement['id_theme'],
-  						'titre' => $avancement['titre_theme'])),
-  				'fait' => $avancement['fait'],
-  				'compris' => $avancement['compris'],
-  				'assimile' => $avancement['assimile']);
+  	function getBySeanceEtudiant($idSeance, $idEtudiant)
+  	{
+	  	$result = $this->executeQuery('SELECT * FROM avancement, theme, exercice 
+	  			WHERE avancement.id_etu = ' . $idEtudiant . '
+				AND avancement.id_seance = ' . $idSeance .'
+				AND avancement.id_exo = exercice.id_exo
+	  			AND exercice.id_theme = theme.id_theme');
+	  	$listeAvancement = array();
+	  	while ($avancement = $this->fetchArray($result)) {
+	  	
+	  			$listeAvancement[] = array('exercice' => array('id' => $avancement['id_exo'],
+																'titre' => $avancement['titre_exo'],
+																'numero' => $avancement['num_exo'],
+																'theme' => array ('id' => $avancement['id_theme'],
+																					'titre' => $avancement['titre_theme'])),
+	  										'fait' => $avancement['fait'],
+	  										'compris' => $avancement['compris'],
+	  										'assimile' => $avancement['assimile']);
+	  	}
+	  	return $listeAvancement;
   	}
-  	return $listeAvancement;
-  }
+  
+  	function getTabByCoursEtudiant($idCours, $idEtudiant)
+  	{
+	  	$result = $this->executeQuery('SELECT * FROM avancement, theme, exercice, seance
+	  			WHERE avancement.id_etu = ' . $idEtudiant . '
+				AND avancement.id_exo = exercice.id_exo
+	  			AND exercice.id_theme = theme.id_theme
+	  			AND theme.id_cours = ' . $idCours);
+	  	
+	  	$listeAvancement = array();
+	  	while ($avancement = $this->fetchArray($result)) {
+	  		 
+	  		$listeAvancement[] = array('exercice' => array('id' => $avancement['id_exo'],
+	  				'titre' => $avancement['titre_exo'],
+	  				'numero' => $avancement['num_exo'],
+	  				'theme' => array ('id' => $avancement['id_theme'],
+	  						'titre' => $avancement['titre_theme'])),
+	  				'fait' => $avancement['fait'],
+	  				'compris' => $avancement['compris'],
+	  				'assimile' => $avancement['assimile']);
+	  	}
+	  	return $listeAvancement;
+  	}
+  
+  	function getTabBySeanceThemeEtudiant($idSeance, $idTheme, $idEtudiant)
+  	{
+	  	$result = $this->executeQuery('SELECT e.id_exo, titre_exo, num_exo, e.id_theme, titre_theme, COALESCE(fait, 0) AS fait, COALESCE(compris, 0) AS compris, COALESCE(assimile, 0) AS assimile
+	  	FROM theme t, exercice e
+	  	LEFT JOIN avancement a ON e.id_exo = a.id_exo
+	  	AND id_seance <= '.$idSeance.'
+	  	AND id_etu = '.$idEtudiant.'
+	  	WHERE e.id_theme = '.$idTheme.'
+	  	GROUP BY e.id_exo');
+	  	 
+	  	$listeAvancement = array();
+	  	 
+	  	while ($avancement = $this->fetchArray($result)) {
+	  			
+	  		$listeAvancement[] = array('exercice' => array('id' => $avancement['id_exo'],
+	  				'titre' => $avancement['titre_exo'],
+	  				'numero' => $avancement['num_exo'],
+	  				'theme' => array ('id' => $avancement['id_theme'],
+	  						'titre' => $avancement['titre_theme'])),
+	  				'fait' => $avancement['fait'],
+	  				'compris' => $avancement['compris'],
+	  				'assimile' => $avancement['assimile']);
+	  	}
+	  	return $listeAvancement;
+  	}
+  
+  	function getTabByThemeEtudiant($idTheme, $idEtudiant)
+  	{  	
+	  	$result = $this->executeQuery('SELECT e.id_exo, titre_exo, num_exo, e.id_theme, titre_theme, COALESCE(fait, 0) AS fait, COALESCE(compris, 0) AS compris, COALESCE(assimile, 0) AS assimile
+	  	FROM theme t, exercice e
+	  	LEFT JOIN avancement a ON e.id_exo = a.id_exo
+	  	AND id_etu = '.$idEtudiant.'
+	  	WHERE e.id_theme = '.$idTheme.'
+	  	GROUP BY e.id_exo');
+	  	
+	  	$listeAvancement = array();
+	  	
+	  	while ($avancement = $this->fetchArray($result)) {
+	  			
+	  		$listeAvancement[] = array('exercice' => array('id' => $avancement['id_exo'],
+	  				'titre' => $avancement['titre_exo'],
+	  				'numero' => $avancement['num_exo'],
+	  				'theme' => array ('id' => $avancement['id_theme'],
+	  						'titre' => $avancement['titre_theme'])),
+	  				'fait' => $avancement['fait'],
+	  				'compris' => $avancement['compris'],
+	  				'assimile' => $avancement['assimile']);
+	  	}
+	  	return $listeAvancement;
+	}
 }
